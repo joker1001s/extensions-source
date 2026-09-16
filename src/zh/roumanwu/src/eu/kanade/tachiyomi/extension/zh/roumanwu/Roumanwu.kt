d25667aa0d42d6fbb39a9832522af8be4569dcb7
joker1001s <132553503+joker1001s@users.noreply.github.com>
2026-09-17 04:32:46 +0800

    Refactor Roumanwu class by removing unused properties
    Removed redundant properties for name, baseUrl, and lang.

--- src/zh/roumanwu/src/eu/kanade/tachiyomi/extension/zh/roumanwu/Roumanwu.kt
+++ src/zh/roumanwu/src/eu/kanade/tachiyomi/extension/zh/roumanwu/Roumanwu.kt
@@ -19,13 +19,7 @@ import java.util.Locale
 
 @Source
 abstract class Roumanwu : HttpSource() {
-
-    override val name = "Roumanwu"
-
-    override val baseUrl = "https://www.rouman5.com"
-
-    override val lang = "zh"
-
+    
     override val supportsLatest = true
 
     override val client = network.client.newBuilder()
